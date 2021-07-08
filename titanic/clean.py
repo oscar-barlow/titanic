492ac6867c232c1c16e4e0c10ddddc2c119c4419
Oscar Barlow <oscar-barlow@users.noreply.github.com>
2021-07-08 10:09:06 +0100

    Do not need data cleaning function; pandas does it, switching model anyhow

--- titanic/clean.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-def clean_titanic_data(data: pd.DataFrame, inplace: bool = False) -> pd.DataFrame:
-    data["SexCode"] = data["Sex"].apply(lambda sex: 1 if sex == "male" else 2)
-    data.drop(columns=['Name', 'Ticket', 'Cabin', 'Embarked', 'Sex'], inplace=inplace)
-    return data
